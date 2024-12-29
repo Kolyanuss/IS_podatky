ce7d6a55d23bc2f6b5ef11ee0471fb1eb8e5b74d
0xKolyanus <mukola.1zt.@gmail.com>
2024-12-29 11:58:52 +0200

    start t ocreate ui for type

--- ui/change_estate_type_ui.py
+++ ui/change_estate_type_ui.py
@@ -3,7 +3,8 @@ from PyQt6.QtWidgets import (
 )
 from PyQt6.QtGui import QFont
 from PyQt6.QtCore import Qt, pyqtSignal
-from ui.styles import apply_style, get_button_style
+from ui.styles import apply_styles, get_button_style
+from ui.utils import create_table_widget
 from app.real_estate_type_repository import RealEstateTypeRepository
 from app.database import Database
 
@@ -14,11 +15,31 @@ class EstateTypeDialog(QDialog):
         super().__init__()
         self.estate_type_repo = RealEstateTypeRepository(database)
         self.year = year
-        self.setWindowTitle("Типи нерухомості")
-        self.setStyleSheet("background-color: #f0f4f8;")
+        self.input_fields = {}
         
+        self.init_ui()
         # self.load_data()
 
+    def init_ui(self):
+        """Ініціалізація основного інтерфейсу"""
+        self.setWindowTitle("Типи нерухомості")
+        self.setStyleSheet("background-color: #f0f4f8;")
+        self.setGeometry(100, 100, 1200, 700)
+        
+        apply_styles(self, ["base", "input_field", "label"])
+        
+        self.table = create_table_widget(8, ["Id"]+[item[1] for item in self.fields_config], self.on_cell_click)
+        
+        input_container = self.create_input_container()
+        
+        button_layout = self.create_buttons()
+        
+        # Компонування основного лейауту
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.table)
+        main_layout.addWidget(input_container)
+        main_layout.addLayout(button_layout)
+        
 
     def load_data(self):
         """Завантаження інформації з бази даних"""
