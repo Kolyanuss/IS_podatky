d8902dddc83d76d11eb695d5be4d976d961b297e
Nikolay_Laptop <mukola.1zt@gmail.com>
2025-02-07 10:05:49 +0200

    additiolan check unique rnokpp value when update

--- ui/add_person_ui.py
+++ ui/add_person_ui.py
@@ -127,6 +127,11 @@ class AddPersonDialog(QWidget):
 
         if all(data[:-2]):
             try:
+                record = self.user_repository.get_record_by_code(data[3])
+                if record:
+                    QMessageBox.warning(self, "Попередження", f"Не вдалося оновити запис: людина з таким кодом вже існує ({record[1]} {record[2]} {record[3]})")
+                    return
+                
                 self.user_repository.update_record(record_id, data)
                 self.edited_signal.emit()
                 # QMessageBox.information(self, "Успіх", "Дані користувача оновлено!")
